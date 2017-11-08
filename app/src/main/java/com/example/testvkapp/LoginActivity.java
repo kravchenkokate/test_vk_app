3ecb05811a5680bb81169e26c425df1a8ab46b0a
Kate <kateryna.kravchenko.mail@gmail.com>
2017-11-08 10:21:40 +0200

    init VKSdk

--- app/src/main/java/com/example/testvkapp/LoginActivity.java
+++ app/src/main/java/com/example/testvkapp/LoginActivity.java
@@ -1,13 +1,79 @@
 package com.example.testvkapp;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String[] scopes = new String[]{
+            VKScope.FRIENDS,
+            VKScope.WALL,
+            VKScope.VIDEO,
+            VKScope.PHOTOS,
+            VKScope.NOHTTPS,
+            VKScope.MESSAGES,
+            VKScope.DOCS
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button signIn = findViewById(R.id.vk_sign_in_btn);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        VKSdk.login(this, scopes);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        VKCallback<VKAccessToken> callback = new VKCallback<VKAccessToken>() {
+
+            @Override
+            public void onResult(VKAccessToken res) {
+
+                new AlertDialog.Builder(getApplicationContext())
+                        .setTitle("User passed Authorization")
+                        .setMessage("Ok!")
+                        .setPositiveButton("OK", null)
+                        .show();
+            }
+
+            @Override
+            public void onError(VKError error) {
+
+                if(!error.errorMessage.isEmpty()) {
+                    new AlertDialog.Builder(getApplicationContext())
+                            .setTitle(R.string.authorization_failed)
+                            .setMessage(error.errorMessage)
+                            .setPositiveButton("OK", null)
+                            .show();
+                }
+            }
+        };
+
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
